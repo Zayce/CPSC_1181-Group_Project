da241e1566b1032daff6507f1091ca0d2223996c
Zayce <zayced@gmail.com>
2021-09-23 12:03:04 -0700

    First commit, train class initialization.

---
+++ Train.java
@@ -0,0 +1,36 @@
+/**
+ * Train Class
+ * 
+ * @author Zulhelmi (Zoella) Mohamad
+ *
+ */
+
+public class Train {
+	
+	private String name;
+	private int power;
+	
+	public Rectangle(String n,  int p) {
+		this.name = n;
+		this.power = p;
+		
+	public String getName() {
+		return name;
+	}
+	public void setName(String n) {
+		this.name = n;
+	}
+	
+	public String getPower() {
+		return power;
+	}
+	public void setPower(int p) {
+		this.power = p;
+	}
+	
+	
+		
+	}
+	
+	
+}
