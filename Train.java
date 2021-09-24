a3ed451f591eb95ad24333a9d20acd240b4d3f05
OliverZzzz <91283168+OliverZzzz@users.noreply.github.com>
2021-09-24 10:33:26 -0700

    getTotalWeightOfCars() and  getNumberOfCars() created. Cange the type of

--- Train.java
+++ Train.java
@@ -2,17 +2,20 @@
  * Train Class
  * 
  * @author Zulhelmi (Zoella) Mohamad
+ * @author Chendong (Oliver) Zhu
  *
  */
 
- TODO: VSCode needs Junit, JavaDocs and Java debugging to be as good as eclipse
+ //TODO: VSCode needs Junit, JavaDocs and Java debugging to be as good as eclipse
 
 public class Train {
 	
 	private String name;
 	private int power;
-	private int numberOfCars = 0;
-	
+	private int[] cars = null;	// Each freight car is represented by an integer representing how many tons that it
+	weighs
+
+	// constructor
 	public Train (String n,  int p) {
 		this.name = n;
 		this.power = p;
@@ -32,15 +35,37 @@ public class Train {
 		this.power = p;
 	}
 	
-	//get Arrayofcars & get&setWeight Oliver is doing.
-
-	public int getNumberOfCars(){
-		return this.numberOfCars;
+	
+	// this function returns the total weights of the cars
+	// @param weight scope: local variable
+	// should consider null case, which is no car at all
+	public int getTotalWeightOfCars(){
+		int weight = 0;	
+		
+		if (this.cars == null){
+			return weight;
+		}
+		
+		for(int i = 0; i < this.cars.length; i++){
+			weight += this.cars.length;
+		}
+		
+		return weight;
 	}
 
-	//getTotalWeightOfCars Oliver is doing
-	public int getTotalWeightOfCars(){
-		
+
+	// this fuction returns the number of cars
+	// @ param scope: local variable
+	// should consider null case, which is no car at al
+	public int getNumberOfCars(){
+		int number = 0;
+
+		if (this.cars == null){
+			return 0;
+		}
+		else{
+			return this.cars.length;
+		}
 	}
     
 	public int getMaxSpeed(int power){
