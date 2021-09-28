684f50f491f13399b4f2217bcc0e7de9893cce01
OliverZzzz <91283168+OliverZzzz@users.noreply.github.com>
2021-09-27 23:55:30 -0700

    Class complete
    null and empty array should still be considered.

--- Train.java
+++ Train.java
@@ -12,9 +12,13 @@ public class Train {
 	
 	private String name;
 	private int power;
+<<<<<<< Updated upstream
 	//private int weight;
 	private int[] cars = null;	// Each freight car is represented by an integer representing how many tons that it
 	// weighs
+=======
+	private int[] cars = null;	// Each freight car is represented by an integer representing how many tons that it weighs
+>>>>>>> Stashed changes
 
 	// constructor
 	private int numberOfCars = 0;
@@ -39,7 +43,7 @@ public class Train {
 	}
 	
 	
-	// this function returns the total weights of the cars
+	// @return the total weights of the cars
 	// @param weight scope: local variable
 	// should consider null case, which is no car at all
 	public int getTotalWeightOfCars(){
@@ -57,10 +61,16 @@ public class Train {
 	}
 
 
+<<<<<<< Updated upstream
 	// this fuction returns the number of cars
 	// @ param scope: local variable
 	// should consider null case, which is no car at all
 	// if someone enter 0 for cars, weight is fine, but total number is 1, not make sence
+=======
+	// @return the number of cars
+	// @param scope: local variable
+	// should consider null case, which is no car at al
+>>>>>>> Stashed changes
 	public int getNumberOfCars(){
 		int number = 0;
 
@@ -71,11 +81,59 @@ public class Train {
 			return this.cars.length;
 		}
 	}
-    
+	
+    // @return the speed of the train
+	// should consider null case.
 	public int getMaxSpeed(int power){
-		return (power - this.getTotalWeightOfCars());
+		int speed = power - this.getTotalWeightOfCars();
+		
+		if (speed > 150) {
+			speed = 150;
+		}
+		
+		return speed;
 	}
 	
+	// toString method that prints the summary of the train
+	public String toString() {
+		String result = "****Train: " + this.name + " ****";
+		result += "\n Power: " + this.power;
+		
+		return result;
+	}
+	
+	// remove all carss from the train
+	public void removeAllCars() {
+		this.cars = null;
+	}
+	
+	// add cars
+	// consider for null
+	public void addCars(int... weights) {
+		// new array is the concadinate of cars and weights
+		int[] temp = new int[this.cars.length + weights.length - 1];
+		
+		// copying cars to new array
+		for (int i = 0; i < this.cars.length; i++) {
+			temp[i] = this.cars[i];
+		}
+		
+		// adding new weights
+		for(int i = this.cars.length; i < temp.length; i++ ) {
+			temp[i] = weights[i];
+		}
+		
+		// updating the value
+		this.cars = temp;
+	}
+	
+	public void mergeTrains(Train other) {
+		power += other.power;
+		addCars(other.cars);
+		other.cars = [];
+		other.power = 0;
+		
+	}
 
 		
 }
