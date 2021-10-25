7fd2c31ef35f3145a62126e9a4d3cf1c56390bd8
Zayce <zayced@gmail.com>
2021-10-24 18:23:39 -0700

    Ex4 done: ConsoleDrawable Interface to be implmntd

---
+++ Assignment_4/src/ConsoleDrawable.java
@@ -0,0 +1,10 @@
+/** 
+ * @author Zulhelmi (Zoella) Mohamad
+ * @author Chendong (Oliver) Zhu
+ * 
+ * Drawing shapes in console
+ **/
+
+public interface ConsoleDrawable {
+	public String drawForConsole();
+}
