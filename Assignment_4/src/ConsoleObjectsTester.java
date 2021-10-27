80b450c1b962d79c00d32ccdbd2909df8e10d2ee
Zayce <zayced@gmail.com>
2021-10-26 23:06:24 -0700

    Exercise 7 Done

---
+++ Assignment_4/src/ConsoleObjectsTester.java
@@ -0,0 +1,64 @@
+import java.util.Arrays;
+
+/**
+ * @author Zulhelmi (Zoella) Mohamad
+ * @author Chendong (Oliver) Zhu
+ */
+public class ConsoleObjectsTester {
+	final int LENGTH = 5;
+	ConsoleShape shapeArr[] = new ConsoleShape[LENGTH];
+	ConsoleDrawable shapeDrawn[] = new ConsoleDrawable[LENGTH + 3];
+	
+	public static void main(String[] args) {
+		ConsoleObjectsTester prog = new ConsoleObjectsTester();
+		prog.execute();
+	}
+	
+	public ConsoleObjectsTester() {
+		
+		ConsoleRectangle rec1 = new ConsoleRectangle(true, 2, 3);
+		ConsoleRectangle rec2 = new ConsoleRectangle(true, 14, 8);
+		ConsoleRectangle rec3 = new ConsoleRectangle(false, -5, -7);
+		ConsoleRectangle rec4 = new ConsoleRectangle();
+		ConsoleRectangle rec5 = new ConsoleRectangle(true, 8, 14);
+
+		shapeArr[0] = rec1;
+		shapeArr[1] = rec2;
+		shapeArr[2] = rec3;
+		shapeArr[3] = rec4;
+		shapeArr[4] = rec5;
+		
+		ConsoleDrawable recDraw1 = rec1;
+		ConsoleDrawable recDraw2 = rec2;
+		ConsoleDrawable recDraw3 = rec3;
+		ConsoleDrawable recDraw4 = rec4;
+		ConsoleDrawable recDraw5 = rec5;
+
+		shapeDrawn[0] = recDraw1;
+		shapeDrawn[1] = recDraw2;
+		shapeDrawn[2] = recDraw3;
+		shapeDrawn[3] = recDraw4;
+		shapeDrawn[4] = recDraw5;
+		shapeDrawn[5] = new Billboard("BTS should have won the Grammys");
+		shapeDrawn[6] = new Billboard("Object Oriented Programming is actually fun and not at all tedious. For real and serious, this is my own opinion!");
+		shapeDrawn[7] = new Billboard("Writing's not easy. That's why Grammarly can help. This sentence is grammatically correct, but it's wordy, and hard to read. "
+				+ "It undermines the writer's message and the word choice is bland. Grammarly's cutting edge technology helps you craft compelling, understandable writing that makes an impact on your reader. "
+				+ "Much better. Are you ready to give it a try? Installation is simple and free. Visit Grammarly.com today!");
+	}
+	
+	public void execute() {
+		Arrays.sort(shapeArr);
+		
+		System.out.println("Rectangles sorted by area: ");
+		for(int i = 0; i < LENGTH; i++) {
+			System.out.println("Shape " + i + ": \n");
+			System.out.println(shapeArr[i].drawForConsole());
+		}
+		
+		System.out.println("ConsoleDrawable Output: ");
+		for(int i = 0; i < LENGTH + 3; i++) {
+			System.out.println("ConsoleDrawable " + i + ": \n");
+			System.out.println(shapeDrawn[i].drawForConsole());
+		}
+	}
+}
--- Assignment_4/src/ConsoleRectangleTest.java
+++ Assignment_4/src/ConsoleRectangleTest.java
@@ -1,88 +1,88 @@
-import static org.junit.jupiter.api.Assertions.*;
-
-import org.junit.jupiter.api.Test;
-
-/**
- * @author Zulhelmi (Zoella) Mohamad
- * @author Chendong (Oliver) Zhu
- * 
- * Test To see if ConsoleRectangle works
- * include Examples
- *
- */
-class ConsoleRectangleTest {
-
-	final double EPSILON = 0.000001;
-	
-	ConsoleRectangle example1 = new ConsoleRectangle(false, 3, 3);
-	ConsoleRectangle example2 = new ConsoleRectangle(true, 5, 5);
-	ConsoleRectangle example3 = new ConsoleRectangle(false, 4, 7);
-	ConsoleRectangle rec1 = new ConsoleRectangle();
-	ConsoleRectangle rec2 = new ConsoleRectangle(true, 2, 3);
-	ConsoleRectangle rec3 = new ConsoleRectangle(false, 5, 7);
-	ConsoleRectangle rec4 = new ConsoleRectangle(true, 8, 14);
-	ConsoleRectangle rec5 = new ConsoleRectangle(true, 14, 8);
-	Billboard bts = new Billboard("BTS should have won the Grammys");
-	
-	@Test
-	void testToString(){
-		System.out.println(rec1.toString());
-		System.out.println(rec2.toString());
-		System.out.println(rec3.toString());
-		System.out.println(rec4.toString());
-		System.out.println(rec5.toString());
-
-
-	}
-	
-	@Test
-	void testDrawForConsole(){
-		System.out.println("Example 1: \n" + example1.drawForConsole());
-		System.out.println("Example 2: \n" + example2.drawForConsole());
-		System.out.println("Example 3: \n" + example3.drawForConsole());
+//import static org.junit.jupiter.api.Assertions.*;
+//
+//import org.junit.jupiter.api.Test;
+//
+///**
+// * @author Zulhelmi (Zoella) Mohamad
+// * @author Chendong (Oliver) Zhu
+// * 
+// * Test To see if ConsoleRectangle works
+// *
+// */
+//class ConsoleRectangleTest {
+//
+//	final double EPSILON = 0.000001;
+//	
+//	ConsoleRectangle rec1 = new ConsoleRectangle();
+//	ConsoleRectangle rec2 = new ConsoleRectangle(true, 2, 3);
+//	ConsoleRectangle rec3 = new ConsoleRectangle(false, -5, -7);
+//	ConsoleRectangle rec4 = new ConsoleRectangle(true, 8, 14);
+//	ConsoleRectangle rec5 = new ConsoleRectangle(true, 14, 8);
+//	
+//	@Test
+//	void testToString(){
+//		System.out.println(rec1.toString());
+//		System.out.println(rec2.toString());
+//		System.out.println(rec3.toString());
+//		System.out.println(rec4.toString());
+//		System.out.println(rec5.toString());
+//
+//
+//	}
+//	
+//	@Test
+//	void testDrawForConsole(){
+//		System.out.println("Rec 1: \n" + rec1.drawForConsole());
+//		System.out.println("Rec 2: \n" + rec2.drawForConsole());
 //		System.out.println("Rec 3: \n" + rec3.drawForConsole());
 //		System.out.println("Rec 4: \n" + rec4.drawForConsole());
 //		System.out.println("Rec 5: \n" + rec5.drawForConsole());
-		System.out.println("BTS: \n" + bts.drawForConsole());
-
-	}
-	
-	@Test
-	void testGetSetHeightWidthIsFilledIn() {
-		ConsoleRectangle rec4 = new ConsoleRectangle();
-		assertEquals(1, rec4.getHeight());
-		assertEquals(1, rec4.getWidth());
-		assertEquals(false, rec4.isFilledIn());
-		rec4.setHeight(7);
-		rec4.setWidth(10);
-		rec4.setFilledIn(true);
-		assertEquals(7, rec4.getHeight());
-		assertEquals(10, rec4.getWidth());
-		assertEquals(true, rec4.isFilledIn());
-		
-		ConsoleRectangle rec5 = new ConsoleRectangle(true, 2, 3);
-		assertEquals(2, rec5.getHeight());
-		assertEquals(3, rec5.getWidth());
-		assertEquals(true, rec5.isFilledIn());
-		
-		ConsoleRectangle rec6 = new ConsoleRectangle(false, 5, 7);
-		assertEquals(5, rec6.getHeight());
-		assertEquals(7, rec6.getWidth());
-		assertEquals(false, rec6.isFilledIn());
-	}
-	
-	@Test
-	void testGetArea() {
-		assertEquals(1, rec1.getArea(), EPSILON);
-		assertEquals(6, rec2.getArea(), EPSILON);
-		assertEquals(35, rec3.getArea(), EPSILON);
-	}
-	
-	@Test
-	void testGetPerimeter(){
-		assertEquals(4, rec1.getPerimeter());;
-		assertEquals(10, rec2.getPerimeter());
-		assertEquals(24, rec3.getPerimeter());
-	}
-	
-}
+//		
+//		Billboard bts = new Billboard("BTS should have won the Grammys");
+//		System.out.println("BTS: \n" + bts.drawForConsole());
+//
+//		Billboard oop = new Billboard("Object Oriented Programming is actually fun and not at all tedious. For real and serious, this is my own opinion!");
+//		System.out.println("OOP: \n" + oop.drawForConsole());
+//
+//
+//	}
+//	
+//	@Test
+//	void testGetSetHeightWidthIsFilledIn() {
+//		ConsoleRectangle rec4 = new ConsoleRectangle();
+//		assertEquals(1, rec4.getHeight());
+//		assertEquals(1, rec4.getWidth());
+//		assertEquals(false, rec4.isFilledIn());
+//		rec4.setHeight(7);
+//		rec4.setWidth(10);
+//		rec4.setFilledIn(true);
+//		assertEquals(7, rec4.getHeight());
+//		assertEquals(10, rec4.getWidth());
+//		assertEquals(true, rec4.isFilledIn());
+//		
+//		ConsoleRectangle rec5 = new ConsoleRectangle(true, 2, 3);
+//		assertEquals(2, rec5.getHeight());
+//		assertEquals(3, rec5.getWidth());
+//		assertEquals(true, rec5.isFilledIn());
+//		
+//		ConsoleRectangle rec6 = new ConsoleRectangle(false, 5, 7);
+//		assertEquals(5, rec6.getHeight());
+//		assertEquals(7, rec6.getWidth());
+//		assertEquals(false, rec6.isFilledIn());
+//	}
+//	
+//	@Test
+//	void testGetArea() {
+//		assertEquals(1, rec1.getArea(), EPSILON);
+//		assertEquals(6, rec2.getArea(), EPSILON);
+//		assertEquals(35, rec3.getArea(), EPSILON);
+//	}
+//	
+//	@Test
+//	void testGetPerimeter(){
+//		assertEquals(4, rec1.getPerimeter());;
+//		assertEquals(10, rec2.getPerimeter());
+//		assertEquals(24, rec3.getPerimeter());
+//	}
+//	
+//}
