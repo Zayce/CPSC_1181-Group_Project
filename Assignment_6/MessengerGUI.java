cdec6ed35f8e01cb5854f3ea4b3d1d5a7421227d
Zayce <zayced@gmail.com>
2021-11-29 23:31:43 -0800

    Version 2.0 with working event listeners

--- Assignment_6/MessengerGUI.java
+++ Assignment_6/MessengerGUI.java
@@ -1,15 +1,11 @@
 import java.util.ArrayList;
-import java.util.Random;
-import javax.swing.RootPaneContainer;
 import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
-import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.CheckBox;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
@@ -20,20 +16,21 @@ import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
-
 import javafx.scene.text.Font;
-import javafx.scene.text.FontPosture;
-import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
-import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
 
-import Messenger;
 
 /**
  * @author 	Zulhelmi (Zoella) Mohamad
  * @author	Chendong (Oliver) Zhu
+ * 
+ * This is a GUI for the Messenger program.
+ * 
+ * Currently there are only 4 users that are registered, and this program does not have the functionality to add any more users.
+ * We can however send and receive messages from and to the 4 different users.
+ * 
+ * The 4 current registered users are: A, B, Zoey & Oliver. These usernames are case sensitive.
  * 
  */
 
@@ -41,30 +38,36 @@ public class MessengerGUI extends Application {
 	private boolean isUserSelected = false;
 	private String currentUser;
 
-	private ArrayList<String> userNames;
+	private ArrayList<Message> loadedMsgs;
+	private int loadedMsgIndex = 0, loadedMsgSize = 0;
+
 	private Text displayText = new Text("Select A User");
-	private TextField chooseUserField;
-//	final private Font TEXT_AREA_FONT = Font.font(MONOSPACE); //TODO: FIX THIS
+	private TextField chooseUserField, toUserField;
+	final private Font TEXT_AREA_FONT = Font.font("Monospace"); 
 	
 	private Button selectUserButton, nextMsg, loadAllMsg, loadUnreadMsg, sendMsg;
+	private RadioButton smile, written;
 	private Messenger msngr;
+	private TextArea sendMsgArea, readMsgArea;
+
+
 	
 	@Override
 	public void start(Stage primaryStage){
 
 		
 		msngr = new Messenger();
+		MessengerInit();
 		Text enterUser = new Text("Enter Username");
 		chooseUserField = new TextField();
-		Button selectUserButton = new Button("Select");
-		
+		selectUserButton = new Button("Select");
 		selectUserButton.setOnAction(new UsernameSelectorListener());
 		
 		HBox chooseUser = new HBox(5, enterUser, chooseUserField, selectUserButton);
 		chooseUser.setPadding(new Insets(5));
 		chooseUser.setAlignment(Pos.CENTER);
 		
-		TextArea readMsgArea = new TextArea("No Message Displayed");
+		readMsgArea = new TextArea("No Message Displayed");
         readMsgArea.setEditable(false);
 		nextMsg = new Button("Next");
 		nextMsg.setDisable(true);
@@ -73,9 +76,10 @@ public class MessengerGUI extends Application {
 
 		loadAllMsg = new Button("Load All Messages");
 		loadUnreadMsg = new Button("Load Unread Messages");
-		ReadMsgButtonListener rmbl = new ReadMsgButtonListener();
-		loadAllMsg.setOnAction(rmbl);
-		loadUnreadMsg.setOnAction(rmbl);
+		LoadMsgListener lml = new LoadMsgListener();
+		loadAllMsg.setOnAction(lml);
+		loadUnreadMsg.setOnAction(lml);
+		nextMsg.setOnAction(new NextMsgListener());
 
 		HBox loadMsgBox = new HBox(5 ,loadAllMsg, loadUnreadMsg);
 		loadMsgBox.setAlignment(Pos.CENTER);
@@ -86,35 +90,43 @@ public class MessengerGUI extends Application {
 		readMsgBox.setPadding(new Insets(5));
 		
 		
-		
-		
-		
-		TextField toUserField = new TextField();
+		toUserField = new TextField();
+		toUserField.setEditable(false);
 		HBox sendToBox = new HBox(2, new Text("To"), toUserField);
-		TextArea sendMsgArea = new TextArea();
-		sendMsgArea.setDisable(true);
+		sendMsgArea = new TextArea();
+		sendMsgArea.setEditable(false);
 		
+		sendMsgArea.setFont(TEXT_AREA_FONT);
+		readMsgArea.setFont(TEXT_AREA_FONT);
+		toUserField.setFont(TEXT_AREA_FONT);
+		chooseUserField.setFont(TEXT_AREA_FONT);
+		
+
 		ToggleGroup msgStyleGroup = new ToggleGroup();
-		RadioButton smile = new RadioButton("Smile");
-		RadioButton written = new RadioButton("Written");
+		smile = new RadioButton("Smile");
+		written = new RadioButton("Written");
 		smile.setToggleGroup(msgStyleGroup);
 		written.setToggleGroup(msgStyleGroup);
 		written.setSelected(true);
+		MsgOptionListener mol = new MsgOptionListener();
+		smile.setOnAction(mol);
+		written.setOnAction(mol);
 		
 		sendMsg = new Button("Send");
-
+		sendMsg.setOnAction(new SendMsgListener());
 		
 		HBox msgOptionBox = new HBox(2, new Text("Message Type"), smile, written, sendMsg);
 		HBox.setMargin(sendMsg, new Insets(0,0, 0,60));
 		msgOptionBox.setAlignment(Pos.CENTER);
 
-		
-		
+		VBox sendMsgBox = new VBox(2, sendToBox, sendMsgArea, msgOptionBox);
+		sendMsgBox.setAlignment(Pos.CENTER);
+		sendMsgBox.setPadding(new Insets(5));
 		
 		Tab chooseUserTab, readMsgTab, sendMsgTab;
 		chooseUserTab = new Tab("Choose User", chooseUser);
-		readMsgTab = new Tab("Read Message", readMsg);
-		sendMsgTab = new Tab("Send Message", sendMsg);
+		readMsgTab = new Tab("Read Message", readMsgBox);
+		sendMsgTab = new Tab("Send Message", sendMsgBox);
 		
 		TabPane optionTabs = new TabPane(chooseUserTab, readMsgTab, sendMsgTab);
 		optionTabs.setTabClosingPolicy(TabClosingPolicy.UNAVAILABLE);
@@ -138,53 +150,163 @@ public class MessengerGUI extends Application {
 		Application.launch(args);
 	}
 	
+	private void MessengerInit() {
+		msngr.addUser("A");
+		msngr.addUser("B");
+		msngr.addUser("Zoey");
+		msngr.addUser("Oliver");
+		
+		msngr.sendMessage("Zoey", "Oliver", "Foo");
+		msngr.sendMessage("Zoey", "Oliver", "Bar");
+		msngr.sendMessage("Oliver", "Zoey", "Marco Polo");
+		msngr.sendMessage("Zoey", "Oliver", "I don't think I did well on the 2nd Midterm, I didn't finish the coding part :(");
+		
+		msngr.sendMessage("A", "B", "Hello B, this is A");
+		msngr.sendMessage("A", "B", "Hello B, this is still A");
+		msngr.sendMessage("B", "A", "Hi A, I am a human, just like you.");
+
+
+	}
+	
 	private class UsernameSelectorListener implements EventHandler<ActionEvent>{
 		@Override
 		public void handle(ActionEvent e) {
-			if (currentUser == null || currentUser.equals("")) {
-				throw new IllegalArgumentException("Empty or null username");
+			
+			if(chooseUserField.getText().equals("")) {
+				return;
 			}
 			else {
 				currentUser = chooseUserField.getText();
 			}
 			
 			if(msngr.getUserList().contains(currentUser)){
-				displayText.setText(currentUser + " selected.");
+				displayText.setText("Current User: " + currentUser);
 				isUserSelected = true;
+				readMsgArea.setText("No Message Displayed");
+				toUserField.setEditable(true);
+				sendMsgArea.setEditable(true);
+				
 			}
 			else {
 				displayText.setText("Incorrect Username");
+				readMsgArea.setText("No Message Displayed");
+				nextMsg.setDisable(true);
+				sendMsgArea.setEditable(false);
+				toUserField.setEditable(false);
 			}
 		}
 	}
 	
-	private class ReadMsgButtonListener implements EventHandler<ActionEvent>{
+	private class LoadMsgListener implements EventHandler<ActionEvent>{
 
 		@Override
 		public void handle(ActionEvent e) {
-			if(!isUserSelected) {
+			
+			if(!isUserSelected || (currentUser == null) || currentUser.equals("")) {
 				return;
 			}
 			else {
 				nextMsg.setDisable(false);
-				if(e.getSource() == loadAllMsg) {
-					msngr.getReceivedMessages(currentUser);
+			}
+			
+			if(e.getSource() == loadAllMsg) {
+				loadedMsgs = msngr.getReceivedMessages(currentUser);
+			}
+			else if(e.getSource() == loadUnreadMsg){
+				loadedMsgs = msngr.getReceivedMessages(currentUser, Message.Status.unread);
+			}
+			
+			loadedMsgIndex = 0;
+			loadedMsgSize = loadedMsgs.size();
+			displayText.setText(loadedMsgSize + " message(s) loaded");
+			
+			if (loadedMsgSize <= 0) {
+				readMsgArea.setText("No Message Displayed");
+				nextMsg.setDisable(true);
+			}
+			else {
+				readMsgArea.setText(loadedMsgs.get(loadedMsgIndex).toString()); 
+				if (loadedMsgSize > 1) {
+					nextMsg.setDisable(false);
 				}
 				else {
-					msngr.getReceivedMessages(currentUser, Message.Status.unread);
+					nextMsg.setDisable(true);
 				}
 			}
 		}	
 	}
 
-	private class SendMsgListener implements EventHandler<ActionEvent>{
+	private class NextMsgListener implements EventHandler<ActionEvent>{
+
+		@Override
+		public void handle(ActionEvent event) {
+			loadedMsgIndex++;
+			loadedMsgSize--;
+
+			if(loadedMsgIndex >= loadedMsgs.size()) {
+				nextMsg.setDisable(true);
+			}
+			else {
+				nextMsg.setDisable(false);
+				readMsgArea.setText(loadedMsgs.get(loadedMsgIndex).toString()); 
+				
+				if((loadedMsgIndex + 1) >= loadedMsgs.size()){
+					nextMsg.setDisable(true);
+				}
+			}
+			
+
+		}
+	}
+	
+	private class MsgOptionListener implements EventHandler<ActionEvent>{
 
 		@Override
 		public void handle(ActionEvent e) {
+
+			if(e.getSource() == smile) {
+				sendMsgArea.setEditable(false);
+				sendMsgArea.setText(SmileMessage.smile);
+			}
+			else if (e.getSource() == written){
+				sendMsgArea.setEditable(true);
+				sendMsgArea.setText("");
+			}
 			
 		}
 		
 	}
 	
-	
+	private class SendMsgListener implements EventHandler<ActionEvent>{
+
+		@Override
+		public void handle(ActionEvent e) {
+			String sendTo, sendMsg;
+			sendTo = toUserField.getText();
+			
+			if(sendTo == null || sendTo.equals("")) {
+				return;
+			}
+			
+			if(msngr.getUserList().contains(sendTo)) {
+				if(smile.isSelected()) {
+					msngr.sendSmile(currentUser, sendTo);
+					displayText.setText("Smile Successfully Sent");
+				}
+				else if(written.isSelected()) {
+					sendMsg = sendMsgArea.getText();
+					msngr.sendMessage(currentUser, sendTo, sendMsg);
+					displayText.setText("Message Successfully Sent");
+				}
+			}
+			else {
+				displayText.setText("Recipient Username Not Found");
+			}
+			
+
+
+
+		}
+	}
+
 }
